cf6bae2ac42b9eacf217c0dfea351f04c3ae1465
Sebastian <s.kloesel@gmx.de>
2021-12-29 23:42:21 +0100

    Initial Commit

---
+++ Full_Screen_Modal_SwiftUI/Full_Screen_Modal_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Full_Screen_Modal_SwiftUIApp.swift
+//  Full_Screen_Modal_SwiftUI
+//
+//  Created by Sebastian Klösel on 29.12.21.
+//
+
+import SwiftUI
+
+@main
+struct Full_Screen_Modal_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
